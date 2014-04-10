3813dd10d74adef65b82331840f3aad00d6e9abc
Boris <fierval@gmail.com>
2014-04-09 23:15:10 -0700

    Implementation

---
+++ PermuteSimple/Permutation.fs
@@ -0,0 +1,48 @@
+﻿namespace PermuteSimple
+
+open System
+open System.Collections.Generic
+open System.Linq
+open Microsoft.FSharp.Linq
+
+[<AutoOpen>]
+module PermutationsModule =
+    
+    let addOne (num : List<int>) =
+        // find the "unsaturated" postion
+        let zeroOut (num : List<int>) curPos = [0..curPos] |> List.map(fun i -> num.[i] <- 0)
+        let rec addOneRec (num : List<int>) curPos =
+            if num.[curPos] < curPos 
+            then 
+                num.[curPos] <- num.[curPos] + 1
+                zeroOut num (curPos - 1) |> ignore
+            elif curPos = num.Count - 1
+            then
+                zeroOut num curPos |> ignore
+                num.Add(1) |> ignore
+            else
+                addOneRec num (curPos + 1)
+
+        addOneRec num 0
+            
+    /// Generate factoradic numbers of length "len"
+    /// Include 1! and 0 last digit
+    /// http://en.wikipedia.org/wiki/Factorial_number_system
+    let generateFactoradic len =
+            
+        // stores the current number
+        let cur = List<int>()
+        cur.Add(0)
+
+        // first two numbers: 0 and 1!
+
+        [|
+            let yld = cur.ToArray()
+            yield yld.AsEnumerable()
+
+            for i = 1 to len - 1 do
+                addOne cur
+                let yld = cur.ToArray()
+                yield yld.Reverse()               
+        |]
+    
---
+++ PermuteSimple/Script.fsx
@@ -0,0 +1,11 @@
+﻿// Learn more about F# at http://fsharp.net. See the 'F# Tutorial' project
+// for more guidance on F# programming.
+
+#load "Permutation.fs"
+open PermuteSimple
+
+let seqs = generateFactoradic 23
+//
+for s in seqs do
+    printfn "%A" s
+
