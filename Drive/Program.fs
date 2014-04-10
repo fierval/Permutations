34b6074660c77281caf5f171ef8aa778a7581554
Boris <fierval@gmail.com>
2014-04-09 23:12:30 -0700

    Factoradic numbers

---
+++ Drive/Program.fs
@@ -0,0 +1,13 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+
+open PermuteSimple
+
+[<EntryPoint>]
+let main argv = 
+    let seqs = generateFactoradic 5
+
+    for s in seqs do
+        printfn "%A" s
+    
+    0 // return an integer exit code
